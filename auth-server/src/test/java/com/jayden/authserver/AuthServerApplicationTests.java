f94d9b83efb5a58ed4498ca2a0ece07a8cab6223
jayden-lee <gwanggi.lee@gmail.com>
2019-11-10 21:58:37 +0900

    add test code
    obtain access token api

--- auth-server/src/test/java/com/jayden/authserver/AuthServerApplicationTests.java
+++ auth-server/src/test/java/com/jayden/authserver/AuthServerApplicationTests.java
@@ -1,13 +1,43 @@
 package com.jayden.authserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
-class AuthServerApplicationTests {
+public class AuthServerApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    public void getAccessToken() throws Exception {
+        String clientId = "client";
+        String clientSecret = "1234";
+        String username = "user";
+        String password = "1234";
+
+        this.mockMvc.perform(post("/oauth/token")
+            .with(httpBasic(clientId, clientSecret))
+            .param("username", username)
+            .param("password", password)
+            .param("grant_type", "password"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("token_type").exists())
+            .andExpect(jsonPath("access_token").exists());
     }
 
 }
